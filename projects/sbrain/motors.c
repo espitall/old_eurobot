d8161a4d87073208bfa33f7fcea73759b3190dcb
Damien Espitallier <damien.espitallier@gmail.com>
2014-06-04 21:42:10 +0200

    fichiers manquants

---
+++ projects/sbrain/motors.c
@@ -0,0 +1,65 @@
+#include <avr/io.h>
+#include <xmega/pwm/pwm.h>
+#include "motors.h"
+#include "config.h"
+
+static pwm_t pwms;
+
+void motors_init(void)
+{
+  //init pwm
+  PORTE.DIRSET = (1 << 2) | (1 << 3);
+  PORTH.DIRSET = 0x0F;
+  pwm_init(&pwms,(TC0_t *)&TCE0,TC_CLKSEL_DIV1_gc, TC0_CCCEN_bm | TC0_CCDEN_bm);
+  pwm_set_period(&pwms, MOTORS_PWM_PERIOD);
+
+  //put robot in a safety state
+  motors_set_consign(0, 0);
+}
+
+void motors_set_consign(int32_t left, int32_t right)
+{
+  //compute max and min
+  if(left > MOTORS_PWM_PERIOD)
+  {
+    left = MOTORS_PWM_PERIOD;
+  }
+  if(left < -MOTORS_PWM_PERIOD)
+  {
+    left = -MOTORS_PWM_PERIOD;
+  }
+  if(right > MOTORS_PWM_PERIOD)
+  {
+    right = MOTORS_PWM_PERIOD;
+  }
+  if(right < -MOTORS_PWM_PERIOD)
+  {
+    right = -MOTORS_PWM_PERIOD;
+  }
+
+  if(right >= 0)
+  {
+    PORTH.OUTCLR = (1 << 0);
+    PORTH.OUTSET = (1 << 1);
+  }
+  else
+  {
+    PORTH.OUTSET = (1 << 0);
+    PORTH.OUTCLR = (1 << 1);
+  }
+
+  if(left >= 0)
+  {
+    PORTH.OUTSET = (1 << 2);
+    PORTH.OUTCLR = (1 << 3);
+  }
+  else
+  {
+    PORTH.OUTCLR = (1 << 2);
+    PORTH.OUTSET = (1 << 3);
+  }
+
+  //set pwm
+  pwm_set_output(&pwms, 'D', left);
+  pwm_set_output(&pwms, 'C', right);
+}
